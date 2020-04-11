4ffee2bb26fa2a4c08693703482269f41d406134
Uwe Riegel <uriegel@hotmail.de>
2020-04-11 08:25:52 +0200

    UwebSocket setup

---
+++ Tester/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+open System.IO
+
+let configuration = Configuration.create {
+    Configuration.createEmpty() with 
+        Port = 9865
+        Requests = [ Static.useStatic (Path.Combine (Directory.GetCurrentDirectory (), "webroot")) "/" ]
+}
+let server = Server.create configuration 
+server.start ()
+Console.ReadLine () |> ignore
+server.stop ()
