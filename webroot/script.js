d17408dbf6176346c4fc640950f49df7ac53233c
Uwe Riegel <uriegel@hotmail.de>
2020-04-15 21:03:14 +0200

    onopen

--- webroot/script.js
+++ webroot/script.js
@@ -1,7 +1,7 @@
 //const ws = new WebSocket("ws://ubuntu:9865/websocketurl", ["Eigenes", "Zweites"])
 const ws = new WebSocket("ws://ubuntu:9865/websocketurl")
 
-
+ws.onopen = () => ws.send("WS Opened")
 let i = 0
 
 ws.onclose = () => console.log("Closed")
